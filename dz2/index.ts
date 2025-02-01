6d8bfba0e448f4f938e796c23f22e1854b45bf30
sofiia <sofka2708@gmail.com>
2025-02-01 09:12:06 +0200

    New folder and moved files

---
+++ dz2/index.ts
@@ -0,0 +1,24 @@
+import { isPositive } from './ispos';
+
+function generateRandomNumberAndReact(): void {
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+// Author is Sofiia Hryshyna, email is sofka2708@gmail.com
+console.log(`Generated number: ${randomNumber}`);
+
+    if (randomNumber >= 6) {
+        console.log("😊 Yay! This is a happy number!");
+    } else {
+        console.log("😢 Oh no! This is a sad number.");
+    }
+    console.log("Thanks for playing!");
+}
+ 
+function isEven(number: number): boolean {
+    return number % 2 === 0;
+}
+
+const testNumber = 7;
+console.log(`${testNumber} is ${isEven(testNumber) ? "even" : "odd"}.`);
+
+const testNumber2 = -5;
+console.log(`${testNumber} is ${isPositive(testNumber) ? "positive" : "negative"}.`);
