f6d40e1eb975e84bf90e328591f520952d93ec70
sofiia <sofka2708@gmail.com>
2025-02-18 22:40:51 +0200

    Fixed issues

--- dz7/src/arrow-function.ts
+++ dz7/src/arrow-function.ts
@@ -1,9 +1,16 @@
 const sumArray = (arr: (number | string)[]): number =>
-    arr.reduce((sum: number, current: string | number) => sum + Number(current), 0);
+    arr.reduce((sum: number, current: string | number) => {
+        const num = Number(current);
+        if (isNaN(num)) {
+            console.log(current + ' is not a number');
+            return sum;
+        }
+        return sum + num;
+    }, 0);
 
-const numbersArray2: number[] = [14, 26, 654, 0, 100];
-const stringsArray21: string[] = ["LALALA", "Monday", "30", "Again", "cat"];
-const stringsArray22: string[] = ["650", "540", "67890", "0", "33"];
+const numbersArray2 = [14, 26, 654, 0, 100];
+const stringsArray21 = ["LALALA", "Monday", "30", "Again", "cat"];
+const stringsArray22 = ["650", "540", "67890", "0", "33"];
 
 console.log(sumArray(numbersArray2));
 console.log(sumArray(stringsArray21));
--- dz7/src/functions.ts
+++ dz7/src/functions.ts
@@ -1,12 +1,17 @@
 function calculateSum(arr: (number | string)[]): number {
     return arr.reduce(function (sum: number, current: number | string): number {
-        return sum + Number(current);
+        const num = Number(current);
+        if (isNaN(num)) {
+            console.log(current + ' is not a number');
+            return sum;
+        }
+        return sum + num;
     }, 0);
 }
 
-const numbersArray: number[] = [14, 26, 654, 0, 100];
-const stringsArray: string[] = ["LALALA", "Monday", "30", "Again", "cat"];
-const stringsArray2: string[] = ["650", "540", "67890", "0", "33"];
+const numbersArray = [14, 26, 654, 0, 100];
+const stringsArray = ["LALALA", "Monday", "30", "Again", "cat"];
+const stringsArray2 = ["650", "540", "67890", "0", "33"];
 
 console.log(calculateSum(numbersArray));
 console.log(calculateSum(stringsArray));
