ce1d5eeb5192b28a883fa43f1d6c907cea1b30b3
sofiia <sofka2708@gmail.com>
2025-02-23 23:36:56 +0200

    Fixed issues in eslint + added  .then()...catch() chain

--- dz8/src/abstraction.ts
+++ dz8/src/abstraction.ts
@@ -4,43 +4,43 @@ export interface UserRole {
 }
 
 export abstract class UserBase {
-    id: number;
-    name: string;
-    email: string;
-    role: UserRole;
+    protected id: number;
+    public name: string;
+    protected email: string;
+    protected role: UserRole;
 
-    constructor(id: number, name: string, email: string, role: UserRole) {
+    public constructor(id: number, name: string, email: string, role: UserRole) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.role = role;
     }
 
-    abstract getInfo(): string;
+    public abstract getInfo(): string;
 }
 
 export class User extends UserBase {
-    constructor(id: number, name: string, email: string) {
-        super(id, name, email, { roleName: "User", accessLevel: 1 });
+    public constructor(id: number, name: string, email: string) {
+        super(id, name, email, { roleName: 'User', accessLevel: 1 });
     }
 
-    getInfo(): string {
+    public getInfo(): string {
         return `User: ${this.name} (${this.email}) - Role: ${this.role.roleName}`;
     }
 }
 
 
 export class Admin extends User {
-    constructor(id: number, name: string, email: string) {
+    public constructor(id: number, name: string, email: string) {
         super(id, name, email);
-        this.role = { roleName: "Admin", accessLevel: 5 }; 
+        this.role = { roleName: 'Admin', accessLevel: 5 };
     }
 
-    banUser(user: User): string {
+    public banUser(user: User): string {
         return `Admin ${this.name} banned user ${user.name}.`;
     }
 
-    getInfo(): string {
+    public getInfo(): string {
         return `Admin: ${this.name} (${this.email}) - Role: ${this.role.roleName}, Access Level: ${this.role.accessLevel}`;
     }
 }
--- dz8/src/interfaces.ts
+++ dz8/src/interfaces.ts
@@ -1,48 +1,44 @@
 export interface Geo {
     lat: string;
     lng: string;
-  }
-  
-  export interface Address {
+}
+export interface Address {
     street: string;
     city: string;
     geo: Geo;
-  }
-  
-  export interface User {
+}
+export interface User {
     id: number;
     name: string;
     username: string;
     address: Address;
-  }
-  
-  export class UserSummary {
-    id: number;
-    fullName: string;
-    location: string;
-    
-  
-    constructor(user: User) {
-      this.id = user.id;
-      this.fullName = `${user.name} (${user.username})`;
-      const coordinates = `${user.address.geo.lat}, ${user.address.geo.lng}`;
-      this.location = `${user.address.street}, ${user.address.city}, ${coordinates}`;
-    }
-  
-    greet(): string {
-      return `Welcome, dear ${this.fullName}! Your location has been identified as: ${this.location}.`;
-    }
-  }
+}
+export class UserSummary {
+    protected id: number;
+    protected fullName: string;
+    protected location: string;
 
-  export async function getJson(userId: number): Promise<User> {
+    public constructor(user: User) {
+        this.id = user.id;
+        this.fullName = `${user.name} (${user.username})`;
+        const coordinates = `${user.address.geo.lat}, ${user.address.geo.lng}`;
+        this.location = `${user.address.street}, ${user.address.city}, ${coordinates}`;
+    }
+
+    public greet(): string {
+        return `Welcome, dear ${this.fullName}! Your location has been identified as: ${this.location}.`;
+    }
+}
+
+export async function getJson(userId: number): Promise<User> {
     const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
     if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
-      }
+    }
     const json = await response.json() as User;
     return json;
-    }
+}
 
-    export function createSummary(user: User): UserSummary {
-        return new UserSummary(user);
-      }
+export function createSummary(user: User): UserSummary {
+    return new UserSummary(user);
+}
