def07ee6bce6798ef3101a57cb62f0118b0cec05
sofiia <sofka2708@gmail.com>
2025-03-09 22:50:22 +0200

    Did dz10. Created unit-tests using jest and mocha

---
+++ dz10/jest/src/index.ts
@@ -0,0 +1,34 @@
+import { AdminUser } from './admin-user';
+import { PremiumUser } from './premium-user';
+import { RegularUser } from './regular-user';
+import { IUser } from './iuser';
+
+
+export function displayUserInfo(user: IUser): void {
+
+    console.log(`User ID: ${user.getId()}`);
+    console.log(`Username: ${user.getUsername()}`);
+    console.log(`Email: ${user.getEmail()}`);
+    console.log(`Role: ${user.getRole()}`);
+
+    if (user instanceof PremiumUser) {
+
+        console.log(`Subscription ends: ${user.getSubscriptionEndDate()}`);
+    } else if (user instanceof AdminUser) {
+
+        console.log(`Admin level: ${user.getAdminLevel()}`);
+    }
+
+}
+
+async function createUsers(): Promise<void> {
+    const regularUser = new RegularUser('1', 'Sofiia Hryshyna', 'sofka@gmail.com');
+    const premiumUser = new PremiumUser('2', 'Jane Dool', 'jane@outlook.com', 6);
+    const adminUser = new AdminUser('3', 'Admin Admin', 'admin@gmail.com', 3);
+
+    displayUserInfo(regularUser);
+    displayUserInfo(premiumUser);
+    displayUserInfo(adminUser);
+}
+
+createUsers();
